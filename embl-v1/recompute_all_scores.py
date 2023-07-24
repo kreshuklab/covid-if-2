13cdd700ea694fd8b4c7ddb9c8f88ab0d8024b41
Constantin Pape <constantin.pape@embl.de>
2023-07-24 20:21:24 +0200

    Update recomputation script

--- embl-v1/recompute_all_scores.py
+++ embl-v1/recompute_all_scores.py
@@ -1,28 +1,29 @@
-import json
 import os
+from glob import glob
+from pathlib import Path
+
+from tqdm import tqdm
 
 from plate_utils import INPUT_ROOT, read_plate_config
 from compute_cell_qc import compute_cell_qc
 from compute_scores import compute_scores
 
-PROCESSED_FILE = "./processed_final_plates.json"
-PROCESSED_FILE_MAB = "./processed_final_plates_mAB.json"
-PROCESSED_FILE_VIBOR = "./processed_plates_vibor.json"
+
+def get_plate_names(config_root):
+    config_files = glob(os.path.join(config_root, "*.json"))
+    plate_names = [Path(ff).stem for ff in config_files]
+    return plate_names
 
 
-def recompute_all_qc(config_root, processed_file):
-    with open(processed_file) as f:
-        plate_names = json.load(f)
-    for name in plate_names:
+def recompute_all_qc(config_root, plate_names):
+    for name in tqdm(plate_names, desc="Recompute cell QC"):
         config = os.path.join(config_root, f"{name}.json")
         plate_config = read_plate_config(config)
         compute_cell_qc(plate_config, verbose=False)
 
 
-def recompute_all_scores(config_root, processed_file):
-    with open(processed_file) as f:
-        plate_names = json.load(f)
-    for name in plate_names:
+def recompute_all_scores(config_root, plate_names):
+    for name in tqdm(plate_names, desc="Recompute analysis"):
         config = os.path.join(config_root, f"{name}.json")
         plate_config = read_plate_config(config)
         compute_scores(plate_config)
@@ -31,18 +32,16 @@ def recompute_all_scores(config_root, processed_file):
 def main():
     if "mAB" in INPUT_ROOT:
         config_root = "./plate_configs/FINAL_DATASETS_mAB"
-        processed_file = PROCESSED_FILE_MAB
     elif "Vibor" in INPUT_ROOT:
         config_root = "./plate_configs/vibor"
-        processed_file = PROCESSED_FILE_VIBOR
     else:
         config_root = "./plate_configs/FINAL_DATASETS"
-        processed_file = PROCESSED_FILE
 
+    plate_names = get_plate_names(config_root)
     recompute_qc = False
     if recompute_qc:
-        recompute_all_qc(config_root, processed_file)
-    recompute_all_scores(config_root, processed_file)
+        recompute_all_qc(config_root, plate_names)
+    recompute_all_scores(config_root, plate_names)
 
 
 if __name__ == "__main__":
